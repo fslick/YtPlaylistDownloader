e01dc7da2a5115eb0f75d29ecce9e6825492b271
fslick <yoru.omg@gmail.com>
2018-09-18 22:57:37 +0200

    first commit

---
+++ PlaylistDownloader.Fs/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open YoutubeExplode
+open YoutubeExplode.Models
+open YoutubeExplode.Models.MediaStreams
+
+let client = YoutubeClient()
+
+let getPlaylist playlistId = client.GetPlaylistAsync playlistId |> Async.AwaitTask
+
+let getVideos (playlist: Playlist) = playlist.Videos
+
+let sanitizeFilename (filename: string) = 
+    let invalids = System.IO.Path.GetInvalidFileNameChars()
+    String.Join("_", filename.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.')
+
+let downloadVideo (video: Video) = async {
+    let! streamInfoSet = client.GetVideoMediaStreamInfosAsync video.Id |> Async.AwaitTask
+    let streamInfo = streamInfoSet.Audio.WithHighestBitrate()
+    let filename = video.Title |> sanitizeFilename
+    // do! client.DownloadMediaStreamAsync(streamInfo, filename) |> Async.AwaitTask
+    let random = System.Random()
+    do! random.Next(500, 5000) |> System.Threading.Tasks.Task.Delay |> Async.AwaitTask
+    filename |> Console.WriteLine
+}
+
+let playlistId = ""
+
+let playlist = 
+    playlistId
+    |> getPlaylist
+    |> Async.RunSynchronously
+
+printfn "Hello World from F#!"
---
+++ PlaylistDownloader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Models;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace PlaylistDownloader
+{
+    //a
+    class PlaylistDownloader
+    {
+        private static readonly YoutubeClient client = new YoutubeClient();
+
+        static void Main(string[] args)
+        {
+            Main().GetAwaiter().GetResult();
+        }
+
+        static async Task Main()
+        {
+            string playlistId = "";
+            var playlist = await client.GetPlaylistAsync(playlistId);
+            await DownloadVideo(playlist.Videos[0]);
+        }
+
+        static async Task DownloadVideo(Video video)
+        {
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(video.Id);
+
+            var streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+            string fileName = SanitizeFileName(video.Title);
+            string extension = streamInfo.Container.GetFileExtension();
+            //await client.DownloadMediaStreamAsync(streamInfo, $"{fileName}.{extension}");
+            await Task.Delay(5000);
+        }
+
+        static string SanitizeFileName(string filename)
+        {
+            var invalids = System.IO.Path.GetInvalidFileNameChars();
+            return String.Join("_", filename.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
+        }
+    }
+}
